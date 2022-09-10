ec57845d171e6a945cdfd502f31c4845d3da41bc
Rohit Kumar <sonu12345kumar12345@gmail.com>
2022-09-10 14:18:56 +0530

    first
    Signed-off-by: Rohit Kumar <sonu12345kumar12345@gmail.com>

---
+++ README.md
@@ -0,0 +1,52 @@
+# Sarcastic Twitter Bot
+
+
+### This bot reluctantly answer with sarcastic responses. Uses OpenAI's [GPT-3 API](https://beta.openai.com/playground/p/default-marv-sarcastic-chat) to generate the comments. Hosted on [Heroku](https://www.heroku.com/), and uses [offcial Twitter SDK](https://github.com/twitterdev/twitter-api-typescript-sdk) to interact with Twitter.
+
+
+**Note:** mention @bot_witty only under a tweet like below example.
+
+![example](./images/this.png)
+
+
+###
+
+---
+
+## Getting Stated
+
+### Clone the repository
+    $ git clone https://github.com/rohit1kumar/sarcastic-bot.git
+
+### Install dependencies
+    $ cd sarcastic-bot
+    $ npm install
+
+
+### Add environment variables
+- Visit [OpenAI](https://beta.openai.com/) and get your API key
+- Visit [Twitter](https://developer.twitter.com/en/portal/dashboard) and get get your API keys and tokens.
+- Create a `.env` file in the root directory
+    ```
+    $ cp .env.example .env
+    $ nano .env
+- Now fill the corresponding values in the `.env` file
+
+    ```
+    TWITTER_BEARER_TOKEN=
+    TWITTER_API_KEY=
+    TWITTER_API_SECRET_KEY=
+    TWITTER_ACCESS_TOKEN=
+    TWITTER_ACCESS_TOKEN_SECRET=
+    OPENAI_API_KEY=
+    BOT_USERNAME=
+    ```
+    **Note:** Use the same bot username whose API key and token are being used.
+
+
+
+### Run the bot
+    $ npm start
+
+
+*I have used another library to post the tweet because the official Twitter SDK was not working for me, got some error which I was not able to resolve, hence I used [twit](https://www.npmjs.com/package/twit). If you are able to resolve the issue, please feel free to open a PR.*
---
+++ index.mjs
@@ -0,0 +1,127 @@
+import { Client } from "twitter-api-sdk";
+import Twit from "twit";
+import dotenv from "dotenv";
+dotenv.config();
+import { Configuration, OpenAIApi } from "openai";
+
+/*################################## TWITTER API #########################################*/
+const config = {
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET_KEY,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000,
+    strictSSL: true,
+};
+const T = new Twit(config);     
+
+const client = new Client(process.env.TWITTER_BEARER_TOKEN); //twitter api client
+
+const botName = process.env.BOT_USERNAME; //Use the same Twitter username whose API key and token are being used.
+
+
+
+/*################################## OPENAI API #########################################*/
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY, });
+const openai = new OpenAIApi(configuration);
+
+/*############################# GET THE RESP. FROM OPENAI API  ##########################*/
+async function getJoke(questions) {
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-002",
+            prompt: `Marv is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: ${questions}\nMarv:`,
+            temperature: 0.5,
+            max_tokens: 60,
+            top_p: 0.3,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+        return response.data.choices[0].text;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/*############################# REPLY TO TWITTER API  #################################*/
+async function replyToTweet(joke, author_id, id) {
+    const data = {
+        status: `${joke}`,  //the joke
+        in_reply_to_user_id: author_id, //the id of the user who tweeted
+        in_reply_to_status_id: id,  //the id of the tweet
+        auto_populate_reply_metadata: true, //auto populate the reply metadata
+    };
+    try {
+        const resp = await T.post("statuses/update", data);
+        if (resp) {
+            console.log("replied");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/*############################# GET THE TWEETS FROM TWITTER API  ##########################*/
+
+async function getExitingRule() {   //get the existing rules
+    try {
+        const rules = await client.tweets.getRules();
+        return rules;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function deleteAndSetNewRules() {     //delete the existing rules and set new rules
+
+    try {
+        const rules = await getExitingRule();
+        const ids = rules.data.map((rule) => rule.id);
+        await client.tweets.addOrDeleteRules({
+            delete: {
+                ids: ids,
+            }
+        });
+        await client.tweets.addOrDeleteRules({
+            add: [
+                {
+                    value: `@${botName} has:mentions`
+                }
+            ]
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function getMentionedTweet() {
+    try {
+        console.log("running");
+        await deleteAndSetNewRules();
+        const stream = await client.tweets.searchStream({
+            "tweet.fields": [
+                "author_id",    // The ID of the user who posted the tweet
+                "id",   // The ID of the tweet
+                "in_reply_to_user_id"   // The ID of the user the tweet is replying to
+            ],
+            "expansions": [
+                "referenced_tweets.id.author_id"    // The ID of the user who posted the referenced tweet
+            ]
+        });
+
+        for await (const response of stream) {
+            if (response.data.text.includes(`@${botName}`)) { //check if the tweet contains the bot's username
+                const tweet = JSON.stringify(response.includes.tweets[0].text, null, 2).replace(/(https?:\/\/[^\s]+)/g, '').replace(/"/g, '').trim(); //remove the urls and double quotes from the tweet and trim the spaces
+
+                const joke = await getJoke(tweet); //get the joke from the openai api
+                await replyToTweet(joke, response.data.author_id, response.data.id); // reply to the
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+getMentionedTweet();
